578d4fbaaf67adebec7646a09118ec2f12089458
Ukhanskyi <ukhanskyi@gmail.com>
2018-04-05 08:53:47 +0300

    Fix Bag 1 sound #5

--- src/main/java/seabattlepack/view/Sound.java
+++ src/main/java/seabattlepack/view/Sound.java
@@ -7,7 +7,6 @@ import java.net.MalformedURLException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.util.logging.*;
 
