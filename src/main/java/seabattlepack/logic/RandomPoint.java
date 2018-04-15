349b200322d68b687305ef4daa48da2116cfd479
Ukhanskyi <ukhanskyi@gmail.com>
2018-04-15 20:11:31 +0300

    Fix Vulnerabilities RandomPiont #1

--- src/main/java/seabattlepack/logic/RandomPoint.java
+++ src/main/java/seabattlepack/logic/RandomPoint.java
@@ -1,8 +1,8 @@
 package seabattlepack.logic;
 
 public class RandomPoint {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     RandomPoint() {
         this.x = (int) (Math.random() * 10);
