ce7d2a609c593daf4a905e620c0b98b444821d57
Ivan Danyliv <ivan0407>
2018-04-16 20:01:27 +0300

    add junit test

---
+++ src/test/java/seabattlepack/logic/UserTest.java
@@ -0,0 +1,16 @@
+package seabattlepack.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    User user = new User();
+
+    @Test
+    public void isWin() {
+        boolean actual = user.isWin();
+        boolean expected = false;
+    }
+}
