2636185cc77c578150ddc5b450f331c31f4a74ca
Ukhanskyi <ukhanskyi@gmail.com>
2018-05-14 23:22:11 +0300

    Create test for View #5

--- src/test/java/seabattlepack/view/WindowTest.java
+++ src/test/java/seabattlepack/view/WindowTest.java
@@ -1,6 +1,11 @@
 package seabattlepack.view;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WindowTest {
     Window window;
@@ -10,10 +15,10 @@ public class WindowTest {
         window = new Window();
     }
 
-   /* @Test(expected = IOException.class)
+    @Test(expected = IOException.class)
     public void testCreateTempFile() throws IOException {
         Path tmpDir = Files.createTempDirectory("img");
         tmpDir.toFile().delete();
         Path tmpFile = Files.createTempFile(tmpDir, "icon1", ".png");
-    }*/
+    }
 }
