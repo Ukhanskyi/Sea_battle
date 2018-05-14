642efa48f5e84b25bbf5bcce12a779bc4d51da4d
Ukhanskyi <ukhanskyi@gmail.com>
2018-05-14 23:24:18 +0300

    Create test for View #6

--- src/test/java/seabattlepack/view/SoundTest.java
+++ src/test/java/seabattlepack/view/SoundTest.java
@@ -1,6 +1,11 @@
 package seabattlepack.view;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class SoundTest {
 
@@ -11,11 +16,11 @@ public class SoundTest {
         sound = new Sound();
     }
 
-   /* @Test(expected = IOException.class)
+    @Test(expected = IOException.class)
     public void testCreateTempFile() throws IOException {
         Path tmpDir = Files.createTempDirectory("audio");
         tmpDir.toFile().delete();
         Path tmpFile = Files.createTempFile(tmpDir, "pirate", ".wav");
-    }*/
+    }
 
 }
