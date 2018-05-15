f6a0349a347e48be4fcde332b2e59174050e80f2
Ukhanskyi <ukhanskyi@gmail.com>
2018-05-15 14:04:21 +0300

    Create test for View Utils #1

---
+++ src/test/java/seabattlepack/view/UtilsTest.java
@@ -0,0 +1,45 @@
+package seabattlepack.view;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class UtilsTest {
+    Utils utils;
+
+    @Before
+    public void refreshBattlePlace(){
+        utils = new Utils();
+    }
+
+    @Test (expected = IOException.class)
+    public void refreshBattlePlace_should_check_in_Directory_file_sea() throws IOException {
+        Path tmpDir = Files.createTempDirectory("img");
+        tmpDir.toFile().delete();
+        Path tmpFile = Files.createTempFile(tmpDir, "sea", ".png");
+    }
+
+    @Test (expected = IOException.class)
+    public void refreshBattlePlace_should_check_in_Directory_file_ship() throws IOException {
+        Path tmpDir = Files.createTempDirectory("img");
+        tmpDir.toFile().delete();
+        Path tmpFile = Files.createTempFile(tmpDir, "ship", ".png");
+    }
+
+    @Test (expected = IOException.class)
+    public void refreshBattlePlace_should_check_in_Directory_file_killed() throws IOException {
+        Path tmpDir = Files.createTempDirectory("img");
+        tmpDir.toFile().delete();
+        Path tmpFile = Files.createTempFile(tmpDir, "killed", ".png");
+    }
+
+    @Test (expected = IOException.class)
+    public void refreshBattlePlace_should_check_in_Directory_file_miss() throws IOException {
+        Path tmpDir = Files.createTempDirectory("img");
+        tmpDir.toFile().delete();
+        Path tmpFile = Files.createTempFile(tmpDir, "miss", ".png");
+    }
+}
