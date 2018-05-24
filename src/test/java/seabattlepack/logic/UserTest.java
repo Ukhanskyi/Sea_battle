88ebf77a3dcabbfd9da1ad5f8febb6b0d54b8bd6
Ivan Danyliv <ivan0407>
2018-05-24 22:51:43 +0300

    Create junit test #2

--- src/test/java/seabattlepack/logic/UserTest.java
+++ src/test/java/seabattlepack/logic/UserTest.java
@@ -22,4 +22,15 @@ public class UserTest {
     @Test
     public void possibleDirection_should_Be_NotNull() {assertNotNull(user.possibleDirection(2,5));
     }
+
+    @Test
+    public void getBattlePlace() {
+        assertNotNull(user.getBattlePlace());
+
+    }
+
+    @Test
+    public void attack() {
+        assertNotNull(user.attack(1));
+    }
 }
